dd522a472f2264c1ec0e12c20a1c82648ef352c6
quentin@aristote.fr <quentin@aristote.fr>
2024-12-01 23:26:05 +0100

    add user for remote building

--- config/users.nix
+++ config/users.nix
@@ -1,3 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   personal.user.enable = true;
+  users = {
+    users."nixremote" = {
+      isSystemUser = true;
+      shell = pkgs.busybox-sandbox-shell;
+      group = "nixremote";
+      openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEgohiYF2Dsaq6ImGaslnKJMwpiVtwAaM9cm1tpSRr7t root@kerberos"];
+    };
+    groups.nixremote = {};
+  };
+  nix.settings.trusted-users = ["nixremote"];
 }
