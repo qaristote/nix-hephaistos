60b2aa3b8e6e7fb49e0810521a7a2e1258de541d
quentin@aristote.fr <quentin@aristote.fr>
2025-10-18 11:36:03 +0200

    my-nixpkgs: add db of public ssh keys
    Flake lock file updates:
    
    • Updated input 'my-nixpkgs':
        'github:qaristote/my-nixpkgs/abd7901adc60091bb5a23c7e164f07a4511fa61f' (2025-09-28)
      → 'github:qaristote/my-nixpkgs/25f53f34cbc0fcb8917a0c0d217288b39f951bbe' (2025-10-18)
    • Updated input 'nixos-hardware':
        'github:NixOS/nixos-hardware/3441b5242af7577230a78ffb03542add264179ab' (2025-10-04)
      → 'github:NixOS/nixos-hardware/9ed85f8afebf2b7478f25db0a98d0e782c0ed903' (2025-10-10)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/3bcc93c5f7a4b30335d31f21e2f1281cba68c318' (2025-10-04)
      → 'github:NixOS/nixpkgs/98ff3f9af2684f6136c24beef08f5e2033fc5389' (2025-10-16)

--- config/services/git/default.nix
+++ config/services/git/default.nix
@@ -7,9 +7,11 @@
     createHome = true;
     home = "/srv/git";
     shell = "${pkgs.git}/bin/git-shell";
-    openssh.authorizedKeys.keys = config.users.users.qaristote.openssh.authorizedKeys.keys ++ [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICT+jPcQhtBu4jxNAn54PV2TJ5krCfFnbXsR3OHk72l8 qaristote@dragonfly-g4"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDh2W0Nv76Nnw8TNysOkxVDZpnW0VEptq4u4Rask6zoO qaristote@optiplex-9030"
+    openssh.authorizedKeys.keys = with config.personal.lib.publicKeys.ssh; [
+      latitude-7490
+      precision-3571
+      dragonfly-g4
+      optiplex-9030
     ];
   };
 
